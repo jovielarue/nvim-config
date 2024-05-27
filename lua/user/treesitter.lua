77fa94bdb87b8122f1f71206dcadb55eaee78431
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-05-26 21:47:19 -0600

    added bufferline and lualine configs from lunarvim nvim-from-scratch

---
+++ lua/user/treesitter.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+function M.config()
+	local treesitter = require("nvim-treesitter")
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+		ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "javascript" },
+		ignore_install = { " " },
+		sync_install = false,
+
+		highlight = {
+			enable = true,
+			disable = { "css" },
+		},
+		autopairs = {
+			enable = true,
+		},
+		indent = { enable = true, disable = { "" } },
+
+		context_commentstring = {
+			enable = true,
+			enable_autocmd = false,
+		},
+	})
+end
+
+return M
