09cf829608cba38f2954af75d79ce1a940399248
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-06-27 19:46:30 -0600

    use LSP with nvim-cmp-lsp and set up correctly for auto-import

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -34,20 +34,27 @@ require("mason-lspconfig").setup({
   automatic_installation = true,
 })
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-  return
-end
-
-local opts = {}
-
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 for _, server in pairs(servers) do
-  server = vim.split(server, "@")[1]
-
-  local require_ok, conf_opts = pcall(require, "lsp.settings." .. server)
-  if require_ok then
-    opts = vim.tbl_deep_extend("force", conf_opts, opts)
-  end
-
-  lspconfig[server].setup(opts)
+  require("lspconfig")[server].setup {
+    capabilities = capabilities
+  }
 end
+--
+-- local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+-- if not lspconfig_status_ok then
+--   return
+-- end
+--
+-- local opts = {}
+--
+-- for _, server in pairs(servers) do
+--   server = vim.split(server, "@")[1]
+--
+--   local require_ok, conf_opts = pcall(require, "lsp.settings." .. server)
+--   if require_ok then
+--     opts = vim.tbl_deep_extend("force", conf_opts, opts)
+--   end
+--
+--   lspconfig[server].setup(opts)
+-- end
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -21,8 +21,8 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
-    -- { name = 'nvim_lsp' },
     { name = 'luasnip' }, -- For luasnip users.
+    { name = 'nvim_lsp', max_item_count = 5 },
     -- { name = "codeium" },
     { name = 'path' },
     -- { name = 'cmdline' },
