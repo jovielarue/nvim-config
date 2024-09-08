4ec77a587a45c79b1f803e72f302b797e81ad134
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-09-08 15:18:09 -0600

    update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install git
 
 wget https://github.com/neovim/neovim/releases/download/v0.10.1/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
 sudo mv nvim.appimage /usr/bin/nvim
