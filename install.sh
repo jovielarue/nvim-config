70316ed739e6cd1ea7605b50bd3caa94f060797a
Jovie LaRue <44442031+jovielarue@users.noreply.github.com>
2025-08-22 18:07:48 -0600

    Feat: delete install, edit pre-commit

--- install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install git
-
-wget https://github.com/neovim/neovim/releases/download/v0.10.1/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-sudo mv nvim.appimage /usr/bin/nvim
